ffef660e0e96b2ee69dac7e0a5b9e2aa87ebc5a6
Zach Leatherman <zachleatherman@gmail.com>
2020-07-17 09:36:28 -0500

    Update TLD

--- .eleventy.js
+++ .eleventy.js
@@ -80,7 +80,7 @@ module.exports = function(eleventyConfig) {
 
     // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
     // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
+    // Best paired with the `url` filter: https://www.11ty.dev/docs/filters/url/
 
     // You can also pass this in on the command line using `--pathprefix`
     // pathPrefix: "/",
