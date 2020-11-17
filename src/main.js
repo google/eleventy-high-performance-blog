504a29f94796857b797bab7ba19abc0e2e0804d9
Nick Falcone <nicholas.e.falcone@gmail.com>
2020-11-16 21:22:48 -0500

    Prevent duplicate link rel prefetches and prefetching the current page (#54)
    * Prevent duplicate link rel prefetches and prevent prefetching the current page.
    
    Add two checks against the prospective href to prefetch:
    1. Has the href already been prefetched?
    2. Is the href equal to the current page?
    
    There is not a benefit to duplicate prefetches or prefetching the current page.
    If either check is true, return without prefetching the href.
    
    * Remove unnecessary check for duplicate fetches as prefetch cache should already account for this.
    
    * Include query string in the URL comparison (exclude the fragment).
    
    Co-authored-by: nickFalcone <nfalcone@u.rochester.edu>

--- src/main.js
+++ src/main.js
@@ -70,6 +70,15 @@ function prefetch(e) {
   if (e.target.origin != location.origin) {
     return;
   }
+  /**
+   * Return the given url with no fragment
+   * @param {string} url potentially containing a fragment
+   * @return {string} url without fragment
+   */
+  const removeUrlFragment = (url) => url.split("#")[0];
+  if (removeUrlFragment(window.location.href) === removeUrlFragment(e.target.href)) {
+    return;
+  }
   var l = document.createElement("link");
   l.rel = "prefetch";
   l.href = e.target.href;
