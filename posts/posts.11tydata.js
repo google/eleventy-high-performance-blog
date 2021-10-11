502478264049e94efc29bb6e8ed9ccf939f0d36c
Zoli Veres <zltnvrs@gmail.com>
2021-10-11 11:17:17 +0300

    Prettier formatting for posts.11tydata.js

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -2,20 +2,19 @@ const todaysDate = new Date();
 const isDev = require("../_data/isdevelopment")();
 
 function showDraft(data) {
-    if(isDev) return true;
-    const isDraft = 'draft' in data && data.draft !== false;
-    const isFutureDate = data.page.date > todaysDate;
-    return !isDraft && !isFutureDate;
+  if (isDev) return true;
+  const isDraft = "draft" in data && data.draft !== false;
+  const isFutureDate = data.page.date > todaysDate;
+  return !isDraft && !isFutureDate;
 }
 
-module.exports = ()=> {
-    return {
-        eleventyComputed: {
-            eleventyExcludeFromCollections: data => showDraft(data) ? data.eleventyExcludeFromCollections : true,
-            permalink: data => showDraft(data) ? data.permalink : false,
-            "tags": [
-                "posts"
-              ],
-        }
-    }
-}
+module.exports = () => {
+  return {
+    eleventyComputed: {
+      eleventyExcludeFromCollections: (data) =>
+        showDraft(data) ? data.eleventyExcludeFromCollections : true,
+      permalink: (data) => (showDraft(data) ? data.permalink : false),
+      tags: ["posts"],
+    },
+  };
+};
