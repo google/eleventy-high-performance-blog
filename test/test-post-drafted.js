d1036340085b9a6f7ff316d713312b94d3d95de3
Zoli Veres <zltnvrs@gmail.com>
2021-10-12 09:59:56 +0300

    prettier formatting for draft tests

--- test/test-post-drafted.js
+++ test/test-post-drafted.js
@@ -11,40 +11,39 @@ const isDev = require("../_data/isdevelopment")();
  * They suck, because they need to be changed when the hardcoded post changes.
  * They are useful because I tend to break the things they test all the time.
  */
-  
-  describe("Draft posts", () => {
-    if(isDev) this.skip();
-     describe("draft post", () => {
-        const DRAFT_POST = "_site/posts/fifthpost/index.html";
-        
-        it("w. draft: true should NOT be rendered", () => {
-            var draftfileexists = false;
-            try {
-                if (existsSync(DRAFT_POST)) {
-                    draftfileexists = true;
-                } 
-              } catch(err) {
-                throw err;
-              }
 
-            expect(!draftfileexists).to.be(true);
-        });
-     });
-     describe("draft post from future", () => {
-      const DRAFT_POST_FUTURE = "_site/posts/sixthpostdraft/index.html";
-      
-      it("should NOT be rendered", () => {
-          var draftpostfutureexists = false;
-          try {
-              if (existsSync(DRAFT_POST_FUTURE)) {
-                draftpostfutureexists = true;
-              } 
-            } catch(err) {
-              throw err;
-            }
+describe("Draft posts", () => {
+  if (isDev) this.skip();
+  describe("draft post", () => {
+    const DRAFT_POST = "_site/posts/fifthpost/index.html";
 
-          expect(!draftpostfutureexists).to.be(true);
-      });
-   });
- });
-      
+    it("w. draft: true should NOT be rendered", () => {
+      var draftfileexists = false;
+      try {
+        if (existsSync(DRAFT_POST)) {
+          draftfileexists = true;
+        }
+      } catch (err) {
+        throw err;
+      }
+
+      expect(!draftfileexists).to.be(true);
+    });
+  });
+  describe("draft post from future", () => {
+    const DRAFT_POST_FUTURE = "_site/posts/sixthpostdraft/index.html";
+
+    it("should NOT be rendered", () => {
+      var draftpostfutureexists = false;
+      try {
+        if (existsSync(DRAFT_POST_FUTURE)) {
+          draftpostfutureexists = true;
+        }
+      } catch (err) {
+        throw err;
+      }
+
+      expect(!draftpostfutureexists).to.be(true);
+    });
+  });
+});
