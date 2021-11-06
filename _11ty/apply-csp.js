ef5b2a4b78271c79fcb19273dd3624fa346035e1
Malte Ubl <malte.ubl@gmail.com>
2021-11-06 15:20:08 -0700

    Fix bug in URL generation for _headers file

--- _11ty/apply-csp.js
+++ _11ty/apply-csp.js
@@ -82,22 +82,35 @@ const addCspHash = async (rawContent, outputPath) => {
           # [end csp headers]`;
       }
       const oldCustomHeaders = headers.match(regExp)[2].toString();
-      const CSPPolicy = `Content-Security-Policy: ${CSP.apply().regular.replace("HASHES", hashes.join(" "))}`;
-      // write headers for full path (/blog/index.html) and pretty url (/blog/)
-      // 404.html require a different pattern.
-      const newCustomHeaders = (outputPath != "_site/404.html")
-        ? oldCustomHeaders.concat(
-          "\n", filePath, "\n  ", CSPPolicy,
-          "\n", filePathPrettyURL, "\n  ", CSPPolicy)
-        : oldCustomHeaders.concat(
-          "\n", "/404.html", "\n  ", CSPPolicy,
-          "\n", "/*", "\n  ", CSPPolicy);
-      fs.writeFileSync(headersPath, headers.replace(regExp, `$1${newCustomHeaders}\n$3`));
+      const CSPPolicy = `Content-Security-Policy: ${CSP.apply().regular.replace(
+        "HASHES",
+        hashes.join(" ")
+      )}`;
+      let newCustomHeaders = oldCustomHeaders.concat(
+        "\n",
+        filePath,
+        "\n  ",
+        CSPPolicy
+      );
+      if (filePath != filePathPrettyURL) {
+        newCustomHeaders = newCustomHeaders.concat(
+          "\n",
+          filePathPrettyURL,
+          "\n  ",
+          CSPPolicy
+        );
+      }
+      fs.writeFileSync(
+        headersPath,
+        headers.replace(regExp, `$1${newCustomHeaders}\n$3`)
+      );
     } catch (error) {
-      console.log("[apply-csp] Something went wrong with the creation of the csp headers\n", error);
+      console.log(
+        "[apply-csp] Something went wrong with the creation of the csp headers\n",
+        error
+      );
     }
   }
-
 
   return content;
 };
