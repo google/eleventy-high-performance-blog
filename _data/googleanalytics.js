30bd69513f8d6ad8226ef5c96c9632acea2dd698
Malte Ubl <malte.ubl@gmail.com>
2022-10-29 13:55:22 -0700

    clean

--- _data/googleanalytics.js
+++ _data/googleanalytics.js
@@ -23,8 +23,8 @@ const metadata = require("./metadata.json");
 
 module.exports = function () {
   let id = metadata.googleAnalyticsId;
-  /*if (/Update me/.test(id)) {
+  if (/Update me/.test(id)) {
     id = null;
-  }*/
+  }
   return id;
 };
